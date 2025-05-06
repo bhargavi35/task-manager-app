5f5efadc093657c2fe96a838aafa9aafca98157a
Bhargavi Chella <bhargavibanu3513@gmial.com>
2025-05-06 14:45:32 +0530

    updated env url

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,11 @@
 'use client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-const API_BASE_URL = process.env.BACKEND_URL
 
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    router.replace(`${API_BASE_URL}/register`);
+    router.replace(`/register`);
   }, []);
   return null;
 }
